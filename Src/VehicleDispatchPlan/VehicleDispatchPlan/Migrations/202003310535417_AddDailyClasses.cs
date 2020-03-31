affa7363fc465adc258045a184f05c4eb7716757
TakayukiMurayama <61692157+TakayukiMurayama@users.noreply.github.com>
2020-03-31 14:38:39 +0900

    日別コマ数を追加

---
+++ Src/VehicleDispatchPlan/VehicleDispatchPlan/Migrations/202003310535417_AddDailyClasses.cs
@@ -0,0 +1,38 @@
+﻿namespace VehicleDispatchPlan.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDailyClasses : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.T_DailyClasses",
+                c => new
+                    {
+                        Date = c.DateTime(nullable: false),
+                        DefaultClasses = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Date);
+            
+            CreateTable(
+                "dbo.T_DailyClassesByTrainer",
+                c => new
+                    {
+                        Date = c.DateTime(nullable: false),
+                        No = c.Int(nullable: false),
+                        TrainerName = c.String(nullable: false),
+                        Classes = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Date, t.No });
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.T_DailyClassesByTrainer");
+            DropTable("dbo.T_DailyClasses");
+        }
+    }
+}
